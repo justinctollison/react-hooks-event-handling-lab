3b2026a45c08eb705152a7323ae963890e758714
justinctollison <justinctollison@gmail.com>
2021-07-05 14:03:19 -0700

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import react from "react"
+
+function EyesOnMe () {
+
+    function OnBall(){
+        console.log("Good!")
+    }
+
+    function OffBall(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return (
+        <div>
+            <button onFocus={OnBall} onBlur={OffBall}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import react from "react"
+
+function Keypad () {
+
+    function beepBoop(){
+        console.log("Entering password...")
+    }
+
+    return (
+        <div>
+            <input type="password" onChange={beepBoop}></input>
+        </div>
+    )
+}
+
+export default Keypad;
